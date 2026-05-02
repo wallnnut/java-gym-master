7fe54540a2828173efa82595517a8491b5135e2f
wall <unknown@mail.ru>
2026-05-02 19:47:52 +0300

    feat: final fields, verifications in constructor, add equals and hashcode methods

--- src/ru/yandex/practicum/gym/TrainingSession.java
+++ src/ru/yandex/practicum/gym/TrainingSession.java
@@ -1,21 +1,38 @@
 package ru.yandex.practicum.gym;
 
+import java.util.Objects;
+
 public class TrainingSession {
 
-    //группа
-    private Group group;
-    //тренер
-    private Coach coach;
-    //день недели
-    private DayOfWeek dayOfWeek;
-    //время начала занятия
-    private TimeOfDay timeOfDay;
+    private final Group group;
+    private final Coach coach;
+    private final DayOfWeek dayOfWeek;
+    private final TimeOfDay timeOfDay;
 
     public TrainingSession(Group group, Coach coach, DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+        if (group == null || coach == null || dayOfWeek == null || timeOfDay == null) {
+            throw new IllegalArgumentException("TrainingSession fields cannot be null");
+        }
         this.group = group;
         this.coach = coach;
         this.dayOfWeek = dayOfWeek;
         this.timeOfDay = timeOfDay;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof TrainingSession))
+            return false;
+        TrainingSession that = (TrainingSession) o;
+        return Objects.equals(group, that.group) && Objects.equals(coach, that.coach) && dayOfWeek == that.dayOfWeek
+                && Objects.equals(timeOfDay, that.timeOfDay);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(group, coach, dayOfWeek, timeOfDay);
     }
 
     public Group getGroup() {
